ebe8f7c046c941e5a8bf3a89b0f943b42d074c17
Shaishsingh <shaishsingh14m@gmail.com>
2025-02-07 18:40:11 +0530

    Solution to Simple Calculator

--- Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
+++ Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
@@ -3,10 +3,12 @@ int main(){
     int a,b ,result;
     char ch;
     scanf("%d%d%c",&a,&b,&ch);
-    switch(ch){
+    switch(ch)
         case'+':
         result=a+b;
         prinf("%d",result);
         break;
+        default:
+        printf("Error: Invalid operator.\n");
+        break;
     }
-}
