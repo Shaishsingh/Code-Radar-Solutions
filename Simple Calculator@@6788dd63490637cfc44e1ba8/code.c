f963138223c3144a263c49cb19d27a76d417d50e
Shaishsingh <shaishsingh14m@gmail.com>
2025-02-07 18:57:35 +0530

    Solution to Simple Calculator

--- Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
+++ Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
@@ -3,11 +3,7 @@
 int main(){
     int a, b, result;
     char ch;
-
-    // Read two integers and a character
     scanf("%d %d %c", &a, &b, &ch);
-
-    // Perform operation based on character
     switch(ch){
         case '+':
             result = a + b;
