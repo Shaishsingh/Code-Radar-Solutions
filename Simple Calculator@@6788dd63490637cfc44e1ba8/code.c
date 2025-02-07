f5e99d7d56a88663801eea97d74ed14cf8d2f351
Shaishsingh <shaishsingh14m@gmail.com>
2025-02-07 18:42:01 +0530

    Solution to Simple Calculator

--- Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
+++ Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
@@ -1,14 +1,23 @@
 #include<stdio.h>
+
 int main(){
-    int a,b ,result;
+    int a, b, result;
     char ch;
-    scanf("%d%d%c",&a,&b,&ch);
-    switch(ch)
-        case'+':
-        result=a+b;
-        prinf("%d",result);
-        break;
+
+    // Read two integers and a character
+    scanf("%d %d %c", &a, &b, &ch);
+
+    // Perform operation based on character
+    switch(ch){
+        case '+':
+            result = a + b;
+            printf("%d", result);
+            break;
         default:
-        printf("Error: Invalid operator.\n");
-        break;
+            printf("Error: Invalid operator.\n");
+            break;
     }
+
+    return 0;
+}
+
