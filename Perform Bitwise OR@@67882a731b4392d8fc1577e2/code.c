2e75aa9e927b4289f4ecac873b80978f7320a4d0
Shaish <shaishsingh14m@gmail.com>
2025-02-13 20:10:06 +0530

    Solution to Perform Bitwise OR

---
+++ Perform Bitwise OR@@67882a731b4392d8fc1577e2/code.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+int main() {
+int a = 5, b = 3; // Binary: a = 0101, b = 0011
+int result = a | b; // Result: 0111 (decimal 7)
+printf("Bitwise OR: %d\n", result);
+return 0;
+} 
